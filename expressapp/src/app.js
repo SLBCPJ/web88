253aeee56cbb729e38f15e5f22f69f591f973ba2
SLBCPJ <jiimmypedrazz@gmail.com>
2021-10-03 17:05:25 -0500

    first commit

--- expressapp/src/app.js
+++ expressapp/src/app.js
@@ -2,26 +2,29 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const app = express();
+
 const indexRoutes = require('./routes/index');
 
 //****configuraciones****
+app.set('port',process.env.PORT || 3000);
 mongoose.connect('mongodb+srv://root:toor@cluster0.vnmaf.mongodb.net/Cluster0?retryWrites=true&w=majority')
 .then(db => console.log('Connected'))
 .catch(err => console.log(err)); //conexion a Mongoose
-const port = 3000;
+
 
 //***middlewares***
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 
 
-//****rutas: req->request res->respuesta***
+//****rutas: ***
+app.use('/products/',indexRoutes);
 /*app.get('/', (req,res)=>{
     res.send('Hello World')
  });*/
-app.use('/products/',indexRoutes);
+
 
 //***inicializacion del server***
-app.listen(port, ()=>{
+app.listen(app.get('port'), ()=>{
     console.log('Server Started');
 });
